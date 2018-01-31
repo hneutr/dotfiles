5649e44ccee7d05f81f1ac543649ff6cd67560ca
Hunter Wapman <hunter.wapman@gmail.com>
2018-01-31 14:41:01 -0700

    lib.zsh: changed tabs; added settex; moved  to  and switched to pandoc because its fast

--- zsh/lib.zsh
+++ zsh/lib.zsh
@@ -1,14 +1,24 @@
 # convert a markdown file into a pdf, swapping only the extension
-# todo:
-#	- make it so that it works without an extension on the input
-function topdf() {
-	local output="${1:r}.pdf"
-	markdown-pdf $1 -o $output 2> /dev/null
+# - does not require an extension on the input file
+function setmd() {
+    local filename="${1:r}"
+    local input="${filename}.md"
+    local output="${filename}.pdf"
+    pandoc -s -o $output $input
+}
+
+# convert a latex file into a pdf and open it
+# - does not require an extension on the input file
+function settex() {
+    local filename="${1:r}"
+    local input="${filename}.tex"
+    local output="${filename}.pdf"
+    pdflatex $input && open $output
 }
 
 # fuzzy find into vim (credit to "bag-man/dotfiles/bashrc")
 function fvim() {
-  local IFS=$'\n'
-  local files=($(fzf --reverse --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && vim "${files[@]}"
+    local IFS=$'\n'
+    local files=($(fzf --reverse --query="$1" --multi --select-1 --exit-0))
+    [[ -n "$files" ]] && vim "${files[@]}"
 }
