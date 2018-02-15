3fd95d4e3e0967d83f604206f0e2bc3f6a3e37dc
Hunter Wapman <hunter.wapman@gmail.com>
2018-02-14 20:03:38 -0700

    minor change to colors so its not so noisy

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -2,5 +2,7 @@ if [[ "$(uname)" = "Darwin" ]]; then
 	export CLICOLOR=1
 	export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 else
+    if [[ -f $ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.256dark ]]; then
 	eval `dircolors -b $ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.256dark`
+    fi
 fi
