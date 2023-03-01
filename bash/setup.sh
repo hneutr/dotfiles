fd098767561f2460ab01d225864f8a429f1bb46f
hneutr <hunter.wapman@gmail.com>
2023-03-01 13:06:37 -0700

    20230301

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,3 +1,3 @@
-ln -s $DOTDIR/bash/bash_profile $HOME/.bash_profile
-ln -s $DOTDIR/bash/bashrc $HOME/.bashrc
+ln -s $DOTDIR/bash/bash_profile.sh $HOME/.bash_profile
+ln -s $DOTDIR/bash/rc.sh $HOME/.bashrc
 ln -s $DOTDIR/bash $HOME/.bash
