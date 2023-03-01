32809b8b5970642f6f328f0891e0048b18c2ce22
hneutr <hunter.wapman@gmail.com>
2023-03-01 13:03:32 -0700

    20230301

--- shell/exports.python.sh
+++ shell/exports.python.sh
@@ -4,6 +4,6 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 export MPLCONFIGDIR=$XDG_CONFIG_HOME/matplotlib
 
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-command -v pyenv >/dev/null || eval "$(pyenv init -)"
-command -v pyenv >/dev/null || eval "$(pyenv virtualenv-init -)"
+command -v pyenv >/dev/null && export PATH="$PYENV_ROOT/bin:$PATH"
+command -v pyenv >/dev/null && eval "$(pyenv init -)"
+command -v pyenv >/dev/null && eval "$(pyenv virtualenv-init -)"
