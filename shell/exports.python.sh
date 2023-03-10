1e155fb0009a9f898e371e8dd2441621ab76e5e4
hneutr <hunter.wapman@gmail.com>
2023-03-10 16:47:55 -0700

    20230310

--- shell/exports.python.sh
+++ shell/exports.python.sh
@@ -4,6 +4,9 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 export MPLCONFIGDIR=$XDG_CONFIG_HOME/matplotlib
 
 export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null && export PATH="$PYENV_ROOT/bin:$PATH"
-command -v pyenv >/dev/null && eval "$(pyenv init -)"
-command -v pyenv >/dev/null && eval "$(pyenv virtualenv-init -)"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# pyenv-virtualenv is slow, so change it from a precmd to a prepwd
+# (does mean that the prompt isn't # updated)
+eval "$(pyenv virtualenv-init - | sed s/precmd/prepwd/g)"
