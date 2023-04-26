3201c74aeec95651f51fa595a135527cb69dcb00
hneutr <hunter.wapman@gmail.com>
2023-04-26 15:46:46 -0600

    20230426

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -6,14 +6,6 @@ function _G.escape(s)
     return (s:gsub('[%-%.%+%[%]%(%)%$%^%%%?%*]','%%%1'))
 end
 
-function _G.tbl_update(...)
-    return vim.tbl_extend("force", ...)
-end
-
-function _G.tbl_default(...)
-    return vim.tbl_extend("keep", ...)
-end
-
 function _G.default_args(args, defaults)
     return vim.tbl_extend("keep", {}, args or {}, defaults)
 end
@@ -31,58 +23,8 @@ end
 --------------------------------------------------------------------------------
 --                       improving vim.fn return types                        --
 --------------------------------------------------------------------------------
-function _G.isdirectory(path)
-    return vim.fn.isdirectory(path) == 1
-end
-
 function _G.filereadable(path)
     return vim.fn.filereadable(path) == 1
-end
-
-
---------------------------------------------------------------------------------
--- makes directories for a given path.
--- If the last component of the path has an extension, it won't make that part
---------------------------------------------------------------------------------
-function M.make_directories(path)
-    -- if the path ends in a file, don't make it a directory
-    if vim.fn.fnamemodify(path, ':e') then
-        path = vim.fn.fnamemodify(path, ':h')
-    end
-
-    local current_path = ''
-    for part in vim.gsplit(path, '/') do
-        current_path = _G.joinpath(current_path, part)
-        if not _G.isdirectory(current_path) then
-            vim.cmd("silent! execute '!mkdir " .. current_path .. "'")
-        end
-    end
-end
-
-
-function M.open_path(path, open_command)
-    open_command = open_command or "edit"
-
-    M.make_directories(path)
-
-    if _G.isdirectory(path) then
-        -- if it's a directory, open a terminal at that directory
-        vim.cmd("silent " .. open_command)
-        vim.cmd("silent terminal")
-
-        local term_id = vim.b.terminal_job_id
-
-        vim.cmd("silent call chansend(" .. term_id .. ", 'cd " .. path .. "\r')")
-        vim.cmd("silent call chansend(" .. term_id .. ", 'clear\r')")
-    else
-        vim.cmd("silent " .. open_command .. " " .. path)
-    end
-end
-
-
-function M.write_file(content, file)
-    M.make_directories(file)
-    vim.fn.writefile(content, file)
 end
 
 --------------------------------------------------------------------------------
@@ -210,38 +152,6 @@ function M.run_once(args)
             vim[args.scope][args.key] = true
         end
     end
-end
-
---------------------------------------------------------------------------------
--- strip
--- -----
--- - str: string to strip
--- - char: option, char to remove from left and right sides of string. default is whitespace
--- 
--- strips char from the left/right of string
---------------------------------------------------------------------------------
-function _G.strip(str, char)
-    str = _G.lstrip(str, char)
-    str = _G.rstrip(str, char)
-    return str
-end
-
-function _G.lstrip(str, char)
-    if char then
-        char = _G.escape(char)
-    end
-
-    char = char or '%s'
-    return string.gsub(str, "^" .. char .. "*", "", 1)
-end
-
-function _G.rstrip(str, char)
-    if char then
-        char = _G.escape(char)
-    end
-
-    char = char or '%s'
-    return string.gsub(str, char .. "*$", "", 1)
 end
 
 return M
