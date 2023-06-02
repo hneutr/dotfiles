0762683e77621824c0774e10859c6f8b079627a2
hneutr <hunter.wapman@gmail.com>
2023-06-02 02:13:53 -0600

    20230602

--- python/rc.sh
+++ python/rc.sh
@@ -4,7 +4,7 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 #----------------------------------[ pyenv ]-----------------------------------#
 if [ -x "$(command -v pyenv)" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 
     # pyenv-virtualenv is slow, so change it from a precmd to a prepwd
