9377e591b9af9a86e9fffc684321f013e713df93
hneutr <hunter.wapman@gmail.com>
2024-10-01 09:31:05 -0400

    20240930

--- nvim/ftplugin/js.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({"BufEnter"}, {pattern={ "*.js" }, callback=require'util'.run_once({
-    scope = 'b',
-    key = 'ft_opts_applied',
-    fn = function()
-        vim.bo.tabstop = 4
-        vim.bo.shiftwidth = 4
-        vim.bo.expandtab = true
-    end,
-})})
