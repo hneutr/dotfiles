aafaeab3f470c9695f846f85d75847b2c796a49d
hneutr <hunter.wapman@gmail.com>
2024-10-10 14:59:00 -0400

    20241010

--- shell/util.sh
+++ shell/util.sh
@@ -1,9 +1,3 @@
-function hard_source() {
-    if [ -f "$1" ]; then
-        source $1
-    fi
-}
-
 function OS() {
     if [ "$(uname)" = "Darwin" ]; then
         echo "macos"
@@ -11,23 +5,21 @@ function OS() {
 }
 
 function setup_dotdir() {
-    hard_source $1/setup.sh
-    if [ -d "$1/extra" ]; then
-	      echo $(which fd)
-        for extra in "$(fd 'setup.sh' $1/extra)"; do
-            echo $extra
+    source $1/setup.sh 2> /dev/null
+    local EXTRA_DIR=$1/extra
+    if [ -d $EXTRA_DIR ]; then
+        for extra in $EXTRA_DIR/*/rc.sh; do
             setup_dotdir "$(dirname $extra)"
         done
     fi
 }
 
 function rc_init_dir() {
-    hard_source $1/rc.sh
-    if [ -d "$1/extra" ]; then
-        for extra in "$(fd 'rc.sh' $1/extra)"; do
-            if [ ! -z $extra ]; then
-                rc_init_dir "$(dirname $extra)"
-            fi
+    source $1/rc.sh 2> /dev/null
+    local EXTRA_DIR=$1/extra
+    if [ -d $EXTRA_DIR ]; then
+        for extra in $EXTRA_DIR/*/rc.sh; do
+            rc_init_dir "$(dirname $extra)"
         done
     fi
 }
