e5f6c7079ec9300d4915924cec592c291e69ca25
hneutr <hunter.wapman@gmail.com>
2024-11-03 22:55:35 -0500

    20241103

--- nvim/lua/snips/init.lua
+++ nvim/lua/snips/init.lua
@@ -1,23 +1,20 @@
-local Object = require("util.object")
-
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
 local ps = ls.parser.parse_snippet
 
 local M = {}
 
 M.opts = {
     fill = '-',
+    width = 80,
 }
 
-local WIDTH = 80
-local WIDTH_SMALL = 40
-local DIVIDER_FILL_CHAR = '-'
-local INPUT_FILL_CHAR = ' '
-local COMMENT = '-'
+local function f(fn, input_index)
+    input_index = input_index or 1
+    return ls.function_node(function(args) return fn(#args[1][input_index]) end, {input_index})
+end
 
 --------------------------------------------------------------------------------
 --                                                                            --
@@ -46,144 +43,90 @@ local COMMENT = '-'
 --------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------
---                                   Block                                    --
+--                                   Header                                   --
 --------------------------------------------------------------------------------
-local Block = Object:extend()
-Block.arg_defaults = {
-    comment = COMMENT,
-    divider_fill_char = DIVIDER_FILL_CHAR,
-    input_fill_char = INPUT_FILL_CHAR,
-    width = WIDTH,
-}
+local Header = class()
+Header.before = {}
+Header.beside = {}
 
-function Block:new(args)
-    args = vim.tbl_extend("keep", {}, args or {}, Block.arg_defaults)
+function Header:_init(comment)
+    self:set(comment)
 
-    for k, v in pairs(args) do
-        self[k] = v
+    local parts = List({
+        f(self:side_fill('left')),
+        i(1),
+        f(self:side_fill('right')),
+    })
+
+    if #self.before > 0 then
+        local before = List(self.before):map(function(key) return self[key] end):append("")
+        parts:put(t(before))
+        parts:append(t(List(before):clone():reverse()))
+    end
+
+    return parts
+end
+
+function Header:set(comment)
+    self.width = M.opts.width
+    self.fill = M.opts.fill
+
+    self.comment = comment
+    self.divider = comment .. self.fill:rep(self.width - (2 * #comment)) .. comment
+    self.spacer = comment .. (' '):rep(self.width - (2 * #comment)) .. comment
+    self.field = comment .. " "
+end
+
+function Header:side_fill(side)
+    local s = self
+
+    return function(len)
+        local after = s.beside[side] or ""
+        local before = s.comment
+        local rounder = math.ceil
+
+        if side == 'right' then
+            rounder = math.floor
+            after, before = before, after
+        end
+
+        local width = rounder((s.width - len) / 2) - #before - #after
+        width = math.max(0, width)
+
+        return before .. (s.beside.fill or " "):rep(width) .. after
     end
 end
 
-function Block:divider()
-    local str = self.comment
-    str = str .. self.divider_fill_char:rep(self.width - (2 * #str))
-    str = str .. self.comment
-    return str
-end
+-------------------------------------[ H1 ]-------------------------------------
+local H1 = class(Header)
+H1.before = {"divider", "spacer", "spacer"}
 
-function Block:spacer()
-    local str = self.comment
-    str = str .. string.rep(' ', self.width - (2 * #str))
-    str = str .. self.comment
-    return str
-end
+-------------------------------------[ H2 ]-------------------------------------
+local H2 = class(Header)
+H2.before = {"divider", "spacer"}
 
-function Block:set_text(text)
-    self.text = text or ''
-end
+-------------------------------------[ H3 ]-------------------------------------
+local H3 = class(Header)
+H3.before = {"divider"}
+
+-------------------------------------[ H4 ]-------------------------------------
+local H4 = class(Header)
+H4.beside = {left = '[ ', right = ' ]', fill = '-'}
 
 --------------------------------------------------------------------------------
 --                                  Function                                  --
 --------------------------------------------------------------------------------
-local FunctionBlock = Block:extend()
+local FunctionBlock = class(Header)
 
-function FunctionBlock.underline(args, _, user_args)
-    local text = args[1][1]
-    local obj = user_args[1]
-    return obj.divider_fill_char:rep(#text)
-end
-
-function FunctionBlock:snippet()
+function FunctionBlock:_init(comment)
+    self:set(comment)
     return {
-        t({self:divider(), self.comment .. " "}),
+        t({self.divider, self.field}),
         i(1),
-        t({"", self.comment .. " "}),
-        f(FunctionBlock.underline, {1}, {user_args = {{self}}}),
-        t({"", self.comment .. " ", self:divider()}),
+        t({"", self.field}),
+        f(function(len) return self.fill:rep(len) end),
+        t({"", self.field, self.divider}),
     }
-end
-
---------------------------------------------------------------------------------
---                                   Header                                   --
---------------------------------------------------------------------------------
-local Header = Block:extend()
-
-function Header:before() return {} end
-function Header:after()
-    local before = self:before()
-
-    local after = {}
-    for i = #before, 1, -1 do
-        table.insert(after, before[i])
-    end
-
-    return after
-end
-
-function Header:set_text(text)
-    local total = self.width - #text
-    self.to_fill = {}
-    self.to_fill.left = math.max(math.floor(total / 2), #self.comment)
-    self.to_fill.right = math.max(total - self.to_fill.left, #self.comment)
-end
-
-function Header:side_fill(side)
-    local pad_fn = side == 'right' and string.lpad or string.rpad
-    return pad_fn(self.comment, self.to_fill[side], self.input_fill_char)
-end
-
-function Header.side_fill_fn(args, _, user_args)
-    local text = args[1][1]
-    local obj, side = unpack(user_args)
-    obj:set_text(text)
-    return obj:side_fill(side)
-end
-
-function Header:get_text_snip(text)
-    if text then
-        return t(text)
-    end
-end
-
-function Header:snippet()
-    return {
-        self:get_text_snip(self:before()),
-        f(Header.side_fill_fn, {1}, {user_args = {{self, 'left'}}}),
-        i(1),
-        f(Header.side_fill_fn, {1}, {user_args = {{self, 'right'}}}),
-        self:get_text_snip(self:after()),
-    }
-end
-
--------------------------------------[ H1 ]-------------------------------------
-H1 = Header:extend()
-function H1:before() return {self:divider(), self:spacer(), self:spacer(), ""} end
-
--------------------------------------[ H2 ]-------------------------------------
-H2 = Header:extend()
-function H2:before() return {self:divider(), self:spacer(), ""} end
-
--------------------------------------[ H3 ]-------------------------------------
-H3 = Header:extend()
-function H3:before() return {self:divider(), ""} end
-
--------------------------------------[ H4 ]-------------------------------------
-H4 = Header:extend()
-H4.beside_text = {left = '[ ', right = ' ]'}
-
-function H4:side_fill(side)
-    local beside_text = H4.beside_text[side]
-    local to_fill = self.to_fill[side] - #self.comment - #beside_text
-
-    local str = self.comment .. self.divider_fill_char:rep(to_fill)
-
-    if side == 'right' then
-        str = beside_text .. str:reverse()
-    else
-        str = str .. beside_text
-    end
-
-    return str
 end
 
 -----------------------------------[ Print ]------------------------------------
@@ -193,12 +136,12 @@ end
 
 ----------------------------------[ BigLine ]-----------------------------------
 local function big_line_snippet(str)
-    return str:rpad(WIDTH, M.opts.fill) .. "\n$1"
+    return str:rpad(M.opts.width, M.opts.fill) .. "\n$1"
 end
 
 ---------------------------------[ SmallLine ]----------------------------------
 local function small_line_snippet(str)
-    return str:rpad(WIDTH_SMALL, M.opts.fill) .. "\n$1"
+    return str:rpad(M.opts.width / 2, M.opts.fill) .. "\n$1"
 end
 
 --------------------------------------------------------------------------------
@@ -227,23 +170,22 @@ function FTLoader.load(filetype)
         return
     end
 
-    local ft_strings = vim.tbl_get(vim.g.snip_ft_strings, filetype) or {}
-    local comment_str = vim.bo.commentstring:gsub("%s?%%s$", '')
-
     local snips = List()
 
     pcall(function() snips:extend(require(string.format("snips.%s", filetype))) end)
 
     if filetype ~= 'all' then
-        snips:extend({
-            s("h1", H1({comment = comment_str}):snippet()),
-            s("h2", H2({comment = comment_str}):snippet()),
-            s("h3", H3({comment = comment_str}):snippet()),
-            s("h4", H4({comment = comment_str}):snippet()),
-            s("fnb", FunctionBlock({comment = comment_str}):snippet()),
+        local comment = vim.bo.commentstring:gsub("%s?%%s$", '')
+        local ft_strings = vim.tbl_get(vim.g.snip_ft_strings, filetype) or {}
 
-            ps("bl", big_line_snippet(comment_str)),
-            ps("l", small_line_snippet(comment_str)),
+        snips:extend({
+            s("h1", H1(comment)),
+            s("h2", H2(comment)),
+            s("h3", H3(comment)),
+            s("h4", H4(comment)),
+            s("fnb", FunctionBlock(comment)),
+            ps("bl", big_line_snippet(comment)),
+            ps("l", small_line_snippet(comment)),
             ps("p", print_snippet(ft_strings.print)),
             ps("qp", print_snippet(ft_strings.print, true)),
         })
